6f9c0ffb12c414d82fb4ac8ea1e1d8c7f49a7f18
zubairhmd <zubairhmd84@gmail.com>
2021-11-23 23:19:32 +0600

    parse and toFixed

--- practice.js
+++ practice.js
@@ -4,3 +4,19 @@ var name = "zubair ahmed";  //string
 var number = 50;            //number
 var isCool = true  ;        //boolean
 */
+
+//parseInt
+/*
+var num1 = 3;
+var num2 = 3.99;
+var total = parseInt(num1 + num2);
+console.log(total);
+*/
+
+
+//perseFloat and tofixed
+
+var num1 = 6.96969;
+var num2 = 3.99;
+var total = parseFloat(num1 + num2)
+console.log(total.toFixed(2));
